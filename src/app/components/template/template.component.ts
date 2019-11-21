91bcf28ebcc7ff6ed56c0a0e3903c626deff2afc
wilmer142 <wilmercollege@gmail.com>
2019-11-20 23:09:17 -0500

    Implementing template validations

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
 
+  usuario:Object = {
+    nombre: null,
+    apellido: null,
+    email:null,
+    pais: "",
+    sexo: "",
+    acepta: false
+  }
+
+  paises = [
+    {
+    codigo: "COL",
+    nombre: "Colombia"
+    },
+    {
+      codigo: "CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo: "ESP",
+      nombre: "España"
+    },
+  ]
+
+  sexos:string[] = ["Hombre", "Mujer", "Otro"];
   constructor() { }
 
-  ngOnInit() {
+  guardar(forma:NgForm){
+    console.log(forma);
   }
 
 }
